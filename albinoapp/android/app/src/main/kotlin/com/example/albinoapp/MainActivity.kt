a3eb7f7eab4389c2f462c925bfa3756497e04658
basakrdnz <84276926+basakrdnz@users.noreply.github.com>
2023-03-27 21:36:47 +0300

    uygulamanın giriş ekranı

---
+++ albinoapp/android/app/src/main/kotlin/com/example/albinoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.albinoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ albinoapp/lib/main.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          _buildGridItem(context, 'Kamera', Colors.red, CameraScreen()),
+          _buildGridItem(context, 'Belgeler', Colors.blue, DocumentsScreen()),
+          _buildGridItem(context, 'Galeri', Colors.green, GalleryScreen()),
+          _buildGridItem(context, 'Ayarlar', Colors.yellow, SettingsScreen()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGridItem(
+      BuildContext context, String title, Color color, Widget screen) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => screen));
+      },
+      child: Card(
+        color: color,
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+  List<CameraDescription> cameras = [];
+
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    availableCameras().then((value) {
+      setState(() {
+        cameras = value;
+        _controller = CameraController(cameras[0], ResolutionPreset.medium);
+        _controller.initialize().then((_) {
+          if (!mounted) {
+            return;
+          }
+          setState(() {});
+        });
+      });
+    });
+    _initializeCamera();
+  }
+
+  void _initializeCamera() async {
+    // Kamera listesini al
+    List<CameraDescription> cameras = await availableCameras();
+
+    // İlk kamerayı seç
+    CameraDescription camera = cameras.first;
+
+    // Kamera kontrolörünü oluştur ve başlat
+    _controller = CameraController(camera, ResolutionPreset.high);
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera'),
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // Kamera hazır ise önizlemeyi göster
+            return CameraPreview(_controller);
+          } else {
+            // Kamera hazır olana kadar yükleniyor göster
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera),
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+
+            // Fotoğraf çek ve sonucu al
+            final XFile photo = await _controller.takePicture();
+
+            // Çekilen fotoğrafı başka bir sayfada göstermek için yeni sayfaya geçiş yap
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PhotoPreviewScreen(imagePath: photo.path),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PhotoPreviewScreen extends StatelessWidget {
+  final String imagePath;
+
+  const PhotoPreviewScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Preview'),
+      ),
+      body: Image.file(
+        File(imagePath),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class DocumentsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Documents'),
+      ),
+      body: Center(
+        child: Text('Documents Screen'),
+      ),
+    );
+  }
+}
+
+class GalleryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+      ),
+      body: Center(
+        child: Text('Gallery Screen'),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Text('Settings Screen'),
+      ),
+    );
+  }
+}
